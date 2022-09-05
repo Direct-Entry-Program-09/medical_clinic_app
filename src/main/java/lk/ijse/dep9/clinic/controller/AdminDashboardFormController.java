b7da7dd6127cb67a8531d02b1fbc36c08e144e60
Rashmi <rashmijayasekara00@gmail.com>
2022-09-05 17:05:45 +0530

    Close #7

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
@@ -1,4 +1,19 @@
 package lk.ijse.dep9.clinic.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class AdminDashboardFormController {
+    public Button btnPeopleManagement;
+    public Button btnViewRecords;
+    public Button btnSettings;
+
+    public void btnPeopleManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewRecordsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSettingsOnAction(ActionEvent actionEvent) {
+    }
 }
