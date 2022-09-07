f8556c8942ca8fa594663fd18a4fb7513d8fb354
Rashmi <rashmijayasekara00@gmail.com>
2022-09-07 15:19:21 +0530

    Close #13

--- src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/AdminDashboardFormController.java
@@ -12,20 +12,44 @@ public class AdminDashboardFormController {
     public Button btnPeopleManagement;
     public Button btnViewRecords;
     public Button btnSettings;
+    public Button btnLogout;
 
-    public void btnPeopleManagementOnAction(ActionEvent actionEvent) {
+    public void btnPeopleManagementOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/PeopleManagementAddModifyProfile.fxml"))));
+        stage.setTitle("Open Source MEDICARE: Profile Management");
+        stage.setResizable(false);
+        stage.show();
+        stage.centerOnScreen();
+        Stage owner = (Stage) btnSettings.getScene().getWindow();
+        owner.hide();
+        stage.setOnCloseRequest(windowEvent -> owner.show());
     }
 
     public void btnViewRecordsOnAction(ActionEvent actionEvent) {
+
     }
 
     public void btnSettingsOnAction(ActionEvent actionEvent) throws IOException {
         Scene scene=new Scene(FXMLLoader.load(this.getClass().getResource("/view/settings.fxml")));
         Stage stage=new Stage();
-        stage.setTitle("Open Source Medical Clinic");
+        stage.setTitle("Open Source Medicare");
         stage.setScene(scene);
         stage.show();
         stage.centerOnScreen();
+        Stage owner=(Stage)btnSettings.getScene().getWindow();
+        owner.hide();
+        stage.setOnCloseRequest(windowEvent -> owner.show());
 
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"))));
+        stage.setTitle("Open Source MEDICARE");
+        stage.setResizable(false);
+        stage.show();
+        stage.centerOnScreen();
+        btnLogout.getScene().getWindow().hide();
     }
 }
--- src/main/java/lk/ijse/dep9/clinic/controller/PeopleManagementAddModifyProfileController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/PeopleManagementAddModifyProfileController.java
@@ -1,4 +1,27 @@
 package lk.ijse.dep9.clinic.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
 public class PeopleManagementAddModifyProfileController {
+    public ComboBox cmbUserRole;
+    public TextField txtFullName;
+    public TextField txtUserName;
+    public HBox hBoxField;
+    public ComboBox cmbField;
+    public ComboBox cmbGender;
+    public TextField txtNic;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtPassword;
+    public Button btnSave;
+
+    public void cmbUserRoleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
 }
